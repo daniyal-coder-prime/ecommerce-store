821bba56625bc3dc04d21be528852af417216b0f
daniyal-coder-prime <daniyalcoder69@gmail.com>
2026-04-03 15:28:37 +0500

    learning react router

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,16 +1,31 @@
-// import ProductsPage from './pages/ProductsPage'
-import Counter from './practice/day4'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-
-
   return (
-    <>
-     <div>
-            <Counter/>
-        </div>
-    </>
-  )
+    // Step 1: BrowserRouter wraps EVERYTHING
+    // Nothing works without this wrapper
+    <BrowserRouter>
+
+      {/* Step 2: Routes holds all your route definitions */}
+      <Routes>
+
+        {/* Step 3: Each Route maps a URL to a component */}
+        <Route path='/' element={<HomePage />} />
+        <Route path='/products' element={<ProductsPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/login' element={<LoginPage />} />
+
+        {/* Catch-all: any unknown URL redirects to home */}
+        <Route path='*' element={<Navigate to='/' />} />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
